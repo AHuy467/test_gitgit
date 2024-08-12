922268563c9b45723cd9c218acbe5283bae2efff
AHuy467 <huyla.huflit@gmail.com>
2024-08-12 15:32:33 +0700

    push

---
+++ README.md
@@ -0,0 +1,3 @@
+# test_gitgit
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/test_gitgit/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.test_gitgit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
